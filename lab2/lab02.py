efeb8b1161233b40efc2ee4c337c509c27c6e61d
giuseppeEsposito98 <giuseppeesposito98@libero.it>
2022-11-01 18:05:48 +0100

    mutation + crossover

--- lab2/lab02.py
+++ lab2/lab02.py
@@ -2,18 +2,21 @@ from base64 import decode
 import random 
 import numpy as np
 from tkinter import N
+import sys
 
 # Function for the problem 
 def problem(N, seed=42):
-    """Creates an instance of the problem"""
-
+    """Generates the problem, also makes all blocks generated unique"""
     random.seed(seed)
-    return [
+    blocks_not_unique = [
         list(set(random.randint(0, N - 1) for n in range(random.randint(N // 5, N // 2))))
         for n in range(random.randint(N, N * 5))
     ]
+    blocks_unique = np.unique(np.array(blocks_not_unique, dtype=object))
+    return blocks_unique.tolist()
 
 def checkFeasible_initial(individual, N):
+    '''From np array of Lists and size of problem, returns if it provides a possible solution <type 'Bool'>'''
     goal = set(list(range(N)))
     coverage = set()
     for list_ in individual:
@@ -40,43 +43,52 @@ def checkFeasible_offspring(individual01, N, initial_formulation):
     return False
 
 
-def createIndividual(indexes,len_):
-    individual01 = np.zeros(len_, dtype=int)
-    individual01[indexes] = 1
-    return list(individual01)
+def createIndividual_initial(indexes,len_):
+    '''From list of Indexes, returns mask of the individual <type 'List'>'''
+    individual = np.zeros(len_, dtype=bool)
+    individual[indexes] = True
+    return list(individual)
 
-def createFitness(induvual):
+def createFitness(individual):
     fitness = 0
-    for list_ in induvual:
+    for list_ in individual:
         fitness += len(list_)
-    return fitness 
-
-
+    return fitness
 
 def select_parent(population, tournament_size = 2):
     subset = random.choices(population, k = tournament_size)
     return min(subset, key=lambda i: i [0])
 
 def cross_over(g1,g2, len_):
-    cut = random.randint(0,len_)
-
+    cut = random.randint(0,len_-1)
     return g1[:cut] + g2[cut:]
 # cross_over con più tagli
 
 def mutation(g, len_):
-    #print("g: ", g)
     point = random.randint(0,len_-1)
-    #print("len(g): ", len(g))
-    #print("cut: ", point)
-    #print("new g: ", g[:point] + [1-g[point]] + g[point+1:])
-    #print("len(new_g): ", len(g[:point] + [1-g[point]] + g[point+1:]))
-    return g[:point] + [1-g[point]] + g[point+1:]
+    return g[:point] + [not g[point]] + g[point+1:]
 
-N = [5]
+def calculateMutationProbability(best_fitness, N, thr):
+    distance = abs(N - best_fitness)
+    return 1-(distance/N)
+    if distance < thr:
+        # print("hi prob")
+        # probabilty of mutation 0.9
+        # probability of cross 0.1
+        p=0.9
+    else:
+        # probabilty of mutation 0.1
+        # probability of cross 0.9
+        p = 0.4
+    return p
+
+N = 20
+POPULATION_SIZE = 30
+OFFSPRING_SIZE = 5
 
 #Inital list of lists
-for i in N:
-    initial_formulation = problem(i)
+
+initial_formulation = problem(N)
 
 
 random.seed(42)
@@ -96,44 +108,90 @@ gap = list(range(0,len(initial_formulation)))
 population = list()
 
 # we use a while since if the checks will give always false, i can also have a population that too little in size
-while len(population) != ((len(initial_formulation)//2)+1):
-    indexes = np.random.choice(gap, (len(initial_formulation)//2)+1)
-    individual = np.array(initial_formulation, dtype=object)[indexes]
-    if checkFeasible_initial(individual,5) == True:
-        individual01 = createIndividual(indexes, len(initial_formulation))
-        population.append((createFitness(individual),individual01))
+while len(population) != (POPULATION_SIZE):
+    # list of random indexes
+    # this avoid duplicate samples of the same index when initializing the first individuals
+    individual_random_indexes = random.sample(gap, (len(initial_formulation)//2)+1)
+    # np array of lists based on random indexes
+    individual_lists = np.array(initial_formulation, dtype=object)[individual_random_indexes]
+    # #print(individual_random_indexes)
+    # #print(individual_lists)
+    if checkFeasible_initial(individual_lists,N) == True:
+        individual = createIndividual_initial(individual_random_indexes, len(initial_formulation))
+        population.append((createFitness(individual_lists),individual,"initial"))
 
-for _ in range(3):
-    print("iteration: ", _)
+initial_formulation_np = np.array(initial_formulation, dtype=object)
+
+# print("STARTING")
+# for ind in population:
+#     print(ind[0])
+# print("STARTING")
+
+for _ in range(1000):
+    # print("iteration: ", _)
+    print(f"interation {_} with {population[0][0]} because {population[0][2]}")
+    sum_of_cross = 0
+    sum_of_mut = 0
     offspring_pool = list()
-    print("initial population: ", population)
+    # print("pop length 1: ", len(population))
     i = 0
-    while len(offspring_pool) != 50:
-        print(i)
-        if random.random() < .3:
-            p = select_parent(population) 
-            offspring = mutation(p[1], len(initial_formulation))
+    mutation_probability = calculateMutationProbability(population[0][0], N, 5)
+    while len(offspring_pool) != OFFSPRING_SIZE:
+        reason = ""
+        if random.random() < mutation_probability:
+            p = select_parent(population)
+            sum_of_mut += 1
+            offspring_mask = mutation(p[1], len(initial_formulation))
+            offspring_mask = mutation(offspring_mask, len(initial_formulation))
+            reason = "mutation"
         else:
             p1 = select_parent(population)
             p2 = select_parent(population)
-            offspring = cross_over(p1[1],p2[1], len(initial_formulation))
-        if checkFeasible_offspring(offspring, 5, initial_formulation) == True:
-            offspring01 = createIndividual(indexes, len(initial_formulation))
-            offspring_pool.append((createFitness(offspring01), offspring01))
-        i = i + 1
+            sum_of_cross += 1
+            offspring_mask = cross_over(p1[1],p2[1], len(initial_formulation))
+            reason = "cross"
+        
+        offspring_lists = initial_formulation_np[offspring_mask]
+        if checkFeasible_initial(offspring_lists, N) == True:
+            offspring_pool.append((createFitness(offspring_lists), offspring_mask, reason))
+
+    # print("mut", sum_of_mut)
+    # print("cross", sum_of_cross)
+
     population = population + offspring_pool
-    population.sort(key=lambda x: x[0])
-    population = population[:(len(initial_formulation)//2)+1]
+    unique_population = list()
+    for ind in population:
+        if ind not in unique_population:
+            unique_population.append(ind)
+    unique_population=list(unique_population)
+    
+    # print("pop length 2: ", len(unique_population))
+    unique_population.sort(key=lambda x: x[0])
+    # take the fittest individual
+    population = unique_population[:POPULATION_SIZE]
 
-    print("next population ", population)
+    # print("next population ", population)
+    
+    # print("pop length 3: ", len(population))
+    
+print("END")
+for ind, index in zip(population, range(0,5)):
+    print(ind[0])
+    # print(initial_formulation_np[ind[1]])
+    print(ind[2])
+print("END")
 
+#for _ in range(200):
 
 # for _ in range(200):
-#     p1 = select_parent(population)
-#     p2 = select_parent(population)
-#     offspring = cross_over(p1[1],p2[1], len(initial_formulation))
-#     print(checkFeasible_offspring(offspring, 5, initial_formulation))
-#     print(checkFeasible_offspring(p2[1], 5, initial_formulation))
+# p = select_parent(population)
+# offspring_mask = mutation(p[1], len(initial_formulation))
+# for i, j in zip(p[1], offspring_mask):
+#     if i != j:
+#         print("we have a difference")
+    
+#     print(f"i: {i} and j:{j}")
+
 """
 popolazione --> p1 p2 
 101010
